24d69b275eca3187fba52ae1f21fbf35e51b2a6c
Erik C. Grueter <erikgrueter@gmail.com>
2012-12-08 11:18:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PriceSurvey::Application.config.secret_token = 'f19a53d6c105bc2be41e7e962de132c8b99bb1fc7bb40957ef0fa83bfeb4ee3085078a933c4ae94a8ce2c4c82aafc210b1d5c60e14d9f871247fddd62237a5c7'
